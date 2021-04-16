30a0876d9d77a098452848af2ab6bd9cfa94d470
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2021-04-16 16:15:14 +0300

    made Socket and read variables

--- TCP/Program.cs
+++ TCP/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,26 @@ namespace TCP
     {
         static void Main(string[] args)
         {
+;            const string ip = "127.0.0.1";
+            const int port = 8080;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            tcpSocket.Bind(tcpEndPoint);
+            tcpSocket.Listen(2);
+
+            var listener=tcpSocket.Accept();
+            var buffer = new byte[256];
+            var size = 0;
+            var data = new StringBuilder();
+            do
+            {
+
+            }
+            while (listener.Available > 0);
+
+
         }
     }
 }
